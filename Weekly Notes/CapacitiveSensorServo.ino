e35b11af2e65f1eb445d4a26cf993cf608214847
drase15 <justin.smith@cca.edu>
2017-04-05 17:49:55 -0400

    Add files via upload

---
+++ Weekly Notes/CapacitiveSensorServo.ino
@@ -0,0 +1,39 @@
+#include <CapacitiveSensor.h>
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+
+int potpin = 0;  // analog pin used to connect the potentiometer
+int val;    // variable to read the value from the analog pin
+
+CapacitiveSensor   cs_4_2 = CapacitiveSensor(4,2);        // 10M resistor between pins 4 & 2, pin 2 is sensor pin, add a wire and or foil if desired
+//CapacitiveSensor   cs_4_6 = CapacitiveSensor(4,6);        // 10M resistor between pins 4 & 6, pin 6 is sensor pin, add a wire and or foil
+//CapacitiveSensor   cs_4_8 = CapacitiveSensor(4,8);        // 10M resistor between pins 4 & 8, pin 8 is sensor pin, add a wire and or foil
+
+void setup()                    
+{
+   cs_4_2.set_CS_AutocaL_Millis(0xFFFFFFFF);     // turn off autocalibrate on channel 1 - just as an example
+   Serial.begin(9600);
+   myservo.attach(9);  // attaches the servo on pin 9 to the servo object
+}
+
+void loop()                    
+{
+    long start = millis();
+    long total1 =  cs_4_2.capacitiveSensor(30);
+    //long total2 =  cs_4_6.capacitiveSensor(30);
+    //long total3 =  cs_4_8.capacitiveSensor(30);
+
+    //Serial.print(millis() - start);        // check on performance in milliseconds
+    //Serial.print("\t");                    // tab character for debug windown spacing
+
+    val = Serial.println(total1);            // print sensor output 1
+    //Serial.print("\t");
+    //Serial.print(total2);                  // print sensor output 2
+    //Serial.print("\t");
+    //Serial.println(total3);                // print sensor output 3
+
+    val = map(val, 700, 3800, 0, 179);     // scale it to use it with the servo (value between 0 and 180)
+    myservo.write(val);                  // sets the servo position according to the scaled value
+    delay(15);                             // arbitrary delay to limit data to serial port 
+}
